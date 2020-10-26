5171ba7b1108b8220acd3fd4251e05acae5de46b
EU1182053 <65112935+EU1182053@users.noreply.github.com>
2020-10-26 23:13:47 +0530

    Update pswd_generator.py
    Replace uppercase string with symbol string.

--- pswd_generator.py
+++ pswd_generator.py
@@ -1,7 +1,7 @@
 import string
 import random
 s1 = string.ascii_lowercase
-s2 = string.ascii_uppercase
+s2 = '!@#$%&*+=-/'
 s3 = string.digits
 
 print("".join(map(str, random.sample(s1+s2+s3, 8))))
