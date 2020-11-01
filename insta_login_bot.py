aa42c2029bff010717e961db1717f0173a932739
SIDDHESH DATTA SAWANT <65112935+EU1182053@users.noreply.github.com>
2020-11-01 19:30:04 +0530

    Update insta_login_bot.py
    Deleted my own credentials.

--- insta_login_bot.py
+++ insta_login_bot.py
@@ -9,8 +9,8 @@ class InstaBot:
         self.driver = webdriver.Chrome(executable_path='/usr/lib/chromium-browser/chromedriver')
         self.driver.get('https://www.instagram.com/')
         sleep(2)
-        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys("siddheshs3000")
-        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys("cjhATY53")
+        self.driver.find_element_by_xpath("//input[@name=\"username\"]").send_keys("Your_Details")
+        self.driver.find_element_by_xpath("//input[@name=\"password\"]").send_keys("Your_Details")
         self.driver.find_element_by_xpath('//button[@type="submit"]').click()
         # sleep(10)
         notNowButton = WebDriverWait(self.driver, 15).until(
